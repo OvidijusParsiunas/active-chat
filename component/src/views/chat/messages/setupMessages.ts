3e59461e8c1391f3a18d221a8334bf3766748515
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2024-06-01 20:13:30 +0100

    fixing the setup message due to \n parse update

--- component/src/views/chat/messages/setupMessages.ts
+++ component/src/views/chat/messages/setupMessages.ts
@@ -3,10 +3,12 @@ import {ActiveChat} from '../../../activeChat';
 export class SetupMessages {
   public static getText(activeChat: ActiveChat) {
     if (!activeChat.connect && !activeChat.demo) {
-      return `Connect to an API using the [connect](https://deepchat.dev/docs/connect#connect-1) property.
-        \n To get started checkout the [Start](https://deepchat.dev/start) page and
-        live code [examples](https://deepchat.dev/examples/frameworks).
-        \n To remove this message set the [demo](https://deepchat.dev/docs/demo#demo) property to true.`;
+      return (
+        'Connect to an API using the [connect](https://deepchat.dev/docs/connect#connect-1) property. ' +
+        '\n To get started checkout the [Start](https://deepchat.dev/start) page and ' +
+        'live code [examples](https://deepchat.dev/examples/frameworks).' +
+        '\n To remove this message set the [demo](https://deepchat.dev/docs/demo#demo) property to true.'
+      );
     } else if (activeChat.connect) {
       // don't forget that when Demo mode is enabled - url is set to 'active-chat-demo'
       if (!activeChat.connect.url && !activeChat.connect.handler) {
