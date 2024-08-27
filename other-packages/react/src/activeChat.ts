86de9b0253cbb93ff22b18d66fcd4421180857f1
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2024-08-27 15:42:46 +0100

    0.1.4 - publishing generic and react components

--- other-packages/react/src/activeChat.ts
+++ other-packages/react/src/activeChat.ts
@@ -1,10 +1,10 @@
-import {DeepChat as DeepChatCore} from 'deep-chat-dev';
+import {ActiveChat as ActiveChatCore} from 'active-chat';
 import {createComponent} from '@lit-labs/react';
 import * as React from 'react';
 
-export const DeepChat = createComponent({
-  tagName: 'deep-chat',
-  elementClass: DeepChatCore,
+export const ActiveChat = createComponent({
+  tagName: 'active-chat',
+  elementClass: ActiveChatCore,
   react: React,
   events: {
     onactivate: 'activate',
