f3ca064a40858e99d09a309b80884780dc99a4de
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2025-08-10 19:10:51 +0900

    adding ability to use plugins and katex for remarkable

--- component/src/types/remarkable.ts
+++ component/src/types/remarkable.ts
@@ -5,10 +5,24 @@ export interface RemarkableOptions {
   xhtmlOut?: boolean;
   html?: boolean;
   breaks?: boolean;
-  linkify?: boolean;
   langPrefix?: `language-${string}`;
   linkTarget?: string;
   typographer?: boolean;
   quotes?: string;
   highlight?: (str: string, lang: string) => void;
+  math?: true | {delimiter?: string; options?: KatexOptions};
 }
+
+// https://katex.org/docs/options
+export type KatexOptions = {
+  leqno?: boolean;
+  fleqn?: boolean;
+  throwOnError?: boolean;
+  errorColor?: string;
+  macros?: Record<string, string>;
+  minRuleThickness?: number;
+  colorIsTextColor?: boolean;
+  maxSize?: number;
+  maxExpand?: number;
+  globalGroup?: boolean;
+};
